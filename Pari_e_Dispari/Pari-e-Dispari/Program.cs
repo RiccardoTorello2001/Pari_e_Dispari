f354e456009b1c2752cc52f5feb12fa1b5d2f6b0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-08 09:46:47 +0200

    creazione di un progetto pari e dispari

---
+++ Pari_e_Dispari/Pari-e-Dispari/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pari_e_Dispari
+{
+    class Program
+    {
+        public static void Pari()
+        {
+
+            for (int i = 0; i < 200; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+
+        }
+        public static void Dispari()
+        {
+
+            for (int i = 0; i < 200; i++)
+            {
+                if(i%2!=0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("i numeri pari sono:");
+            Pari();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("i numeri dispari sono:");
+            Dispari();
+            Console.ReadLine();
+        }
+    }
+}
